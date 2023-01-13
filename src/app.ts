ba853e9d9a67ec33481c93dde89af34cc6562169
abdelmonem88 <abdelmonem1888@gmail.com>
2023-01-13 23:40:48 +0200

    classes practise

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
-// Code goes here!
+class Department {
+  id: number;
+  name: string;
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+  printDepName() {
+    console.log(`Department name is ${this.name}`);
+  }
+}
+
+class ITDepartment extends Department {
+  employees: string[];
+  constructor() {
+    super(1, 'IT');
+    this.employees = ['men3m', 'walaa'];
+  }
+
+  addEployees(employee: string) {
+    this.employees?.push(employee);
+  }
+
+  printEployees() {
+    console.log(this.employees);
+  }
+}
+
+const IT = new ITDepartment();
+IT.addEployees('loka');
+IT.printEployees();
